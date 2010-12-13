4bfb2ade6b3af5657b05e361db69adfb48e6e6d5
Mark Meves <mark.meves@gmail.com>
2010-12-13 05:12:07 -0500

    Add lines to wget script to pull the remaining assets (images, css) we will need

--- themes/doctrine-markdown-like/wget-from-site.sh
+++ themes/doctrine-markdown-like/wget-from-site.sh
@@ -13,34 +13,35 @@
 
 # we originally tried to use wget -N but that won't work with -O
 
-# don't grab it till we need it
+# for now, it appears we do not need this one:
 # wget -O static/css/home.css              http://www.doctrine-project.org/css/home.css
 
 wget -O static/css/layout.css            http://www.doctrine-project.org/css/layout.css
 wget -O static/css/main.css              http://www.doctrine-project.org/css/main.css
+wget -O static/css/documentation.css     http://www.doctrine-project.org/css/documentation.css
 
 wget -O static/images/arrows.jpg         http://www.doctrine-project.org/images/arrows.jpg
 wget -O static/images/bg-gradient.jpg    http://www.doctrine-project.org/images/bg-gradient.jpg
 wget -O static/images/logo.jpg           http://www.doctrine-project.org/images/logo.jpg
 wget -O static/images/ul-corner.jpg      http://www.doctrine-project.org/images/ul-corner.jpg
 wget -O static/images/ur-corner.gif      http://www.doctrine-project.org/images/ur-corner.gif
+wget -O static/images/bl-corner.gif      http://www.doctrine-project.org/images/bl-corner.gif
+wget -O static/images/br-corner.gif      http://www.doctrine-project.org/images/br-corner.gif
 wget -O static/images/sidebox.jpg        http://www.doctrine-project.org/images/sidebox.jpg
+wget -O static/images/sidebox-foot.jpg   http://www.doctrine-project.org/images/sidebox-foot.jpg
+wget -O static/images/servergrove.jpg    http://www.doctrine-project.org/images/servergrove.jpg
+
+wget -O static/sf/sf_admin/images/next.png http://www.doctrine-project.org/sf/sf_admin/images/next.png
+
+wget -O static/images/_paypal/btn_donateCC_LG.gif  https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif
+wget -O static/images/_paypal/pixel.gif            https://www.paypal.com/en_US/i/scr/pixel.gif
 
 #
-# others that we may or may not need:
-#
-# br-corner.gif
-# bl-corner.gif
-# ur-corner.gif
-#
-# bullet_white.gif
-# sidebox-foot.jpg
-#
-# sf/sf_admin/images/next.png
-# sf/sf_admin/images/tick.png
-# sf/sf_admin/images/error.png
-#
+# others images mentioned in the stylesheets that we may or may not need:
 #
 # disk.gif
 # error.png
+# bullet_white.gif
+# sf/sf_admin/images/tick.png
+# sf/sf_admin/images/error.png
 #
